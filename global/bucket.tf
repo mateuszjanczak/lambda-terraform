6b7057c71757b65ca9abaa35007f6a4ef589a45d
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-12-30 22:25:17 +0100

    Add lambda and bucket for terraform state

---
+++ global/bucket.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${local.accountId}-bucket"
+}
+
+locals {
+  accountId = data.aws_caller_identity.current.account_id
+}
---
+++ lambda.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket = "040489059668-bucket"
+    key    = "weather_check_lambda"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_iam_role" "iam_weather_check_lambda" {
+  name = "iam_weather_check_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "weather_check_lambda" {
+  filename         = "weather_check_lambda.zip"
+  function_name    = "weather_check_lambda"
+  role             = aws_iam_role.iam_weather_check_lambda.arn
+  handler          = "com.serverless.Handler"
+  source_code_hash = filebase64sha256("weather_check_lambda.zip")
+  runtime          = "java8"
+
+  environment {
+    variables = {
+      API_URL = "https://api.openweathermap.org/data/2.5/weather?q=%CITY%&appid=%APIKEY%&units=metric",
+      API_KEY = "749561a315b14523a8f5f1ef95e45864"
+    }
+  }
+}
